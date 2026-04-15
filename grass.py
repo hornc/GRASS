d53092c75486a371794443ac50edbc70edf1d3fb
Charles Horn <charles.horn@gmail.com>
2026-04-15 19:25:55 +1200

    initial commit of example and very basic parser skeleton

---
+++ grass.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import sys
+from lark import Lark
+
+
+"""
+GRASS programming language experimental parser
+https://en.wikipedia.org/wiki/GRASS_(programming_language)
+
+"""
+
+grass_grammar = """
+?start: (line | comment | _NL)+
+
+line: [label] command [";" command]* [comment]
+
+label: "%" /[A-Z]+/
+command: commandname[modifier] [argument ["," argument]*]
+commandname: /[A-Z]+/
+modifier: "/" /[A-Z]/
+argument: /.+/
+
+comment: "*" /.+/
+
+%import common.WS
+%import common.NUMBER
+%import common.ESCAPED_STRING -> STRING
+%import common.NEWLINE -> _NL
+%ignore WS
+"""
+
+
+
+def main():
+    sourcefile = sys.argv[1]
+    with open(sourcefile, 'r') as f:
+        source = f.read()
+
+    print(source)
+    parser = Lark(grass_grammar)
+    print(parser.parse(source).pretty())
+
+if __name__ == '__main__':
+    main()
