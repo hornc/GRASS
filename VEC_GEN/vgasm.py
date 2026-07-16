9fc89ad29da2181d48dc1eb1915d21434a525791
Charles Horn <charles.horn@gmail.com>
2026-07-16 21:59:11 +1200

    wip Transformer and definitions

--- VEC_GEN/vgasm.py
+++ VEC_GEN/vgasm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import argparse
-from lark import Lark
+from lark import Lark, Token, Transformer
 
 DESC = """
 Vector General display instruction mnemonic compiler 
@@ -19,12 +19,12 @@ vg_grammar = r"""
     statement: arg+
 
     ?arg: CNAME
+        | INDIRECT
         | INT
         | SIGNED_INT
-        | INDIRECT_CMD
         | ascii
 
-    INDIRECT_CMD: "*" CNAME
+    INDIRECT: "*"
 
     ascii: "'" CHAR_TOKEN "'"
         | "'" CHAR_TOKEN CHAR_TOKEN "'"
@@ -48,19 +48,139 @@ vg_grammar = r"""
 """
 
 
+BITS = 16
+MASK = (1 << BITS) - 1
+
+
+REGISTERS = {
+    'PIR': 0x4,
+    'NMR': 0x4,
+    'MCR': 0x5,
+    'XR':  0x8,
+    'YR':  0x9,
+    'ZR':  0xA,
+    'AIR': 0xB,
+    'IOR': 0xC,
+    'ISR': 0xD,
+    'PSR': 0x11,
+    'NMR': 0x12,
+    'CSR': 0x13,
+    'DXR': 0x14,
+    'DYR': 0x15,
+    'DZR': 0x16,
+    'R11R': 0x17,
+    'R12R': 0x18,
+    'R13R': 0x19,
+    'R21R': 0x1A,
+    'R22R': 0x1B,
+    'R23R': 0x1C,
+    'R31R': 0x1D,
+    'R32R': 0x1E,
+    'R33R': 0x1F,
+}
+
+
+INTERRUPTS = {k: v << 7 for k, v in {
+    'MED': 1<<0,
+    'MEC': 1<<1,
+    'MEF': 1<<2,
+    'MET': 1<<3,
+    'MEK': 1<<4,
+    'MES': 1<<5,
+    'MDB': 1<<6,
+    'MPH': 1<<7,
+    'MS1': 1<<8,
+    'MS2': 1<<9,
+    'MS3': 1<<10,
+    'MS4': 1<<11,
+    # 'PB'  # 2 bits
+    'MDR': 1<<14,
+    'MDW': 1<<15,
+}.items()}
+
+
+CHAR_SIZE = {
+    'S0': 0x40,
+    'S1': 0x50,
+    'S2': 0x60,
+    'S3': 0x70,
+    'V' : 0x80,  # Vertical write-direction
+}
+
+
+LOOKUP = {
+    '*'   : 0o100000,  # Indirection
+    'NOP' : 0x0,
+    'T'   : 0x1,
+    'SPC' : 0x2000,
+    'HLT' : 0x3000,
+    'LD'  : 0x4000,
+    'OR'  : 0x5000,
+    'AN'  : 0x6000,
+    'AD'  : 0x7000,
+    'VA'  : 0x1004,
+    'DVYY': 0x1009,
+    'DVXX': 0x100A,
+    'DV3D': 0x100B,
+    'CH'  : 0x100F,
+} | REGISTERS | INTERRUPTS | CHAR_SIZE
+
+
+class VGTransformer(Transformer):
+    def __init__(self, outfile, listfile=None):
+        super().__init__()
+        self.out = outfile
+        self.list = listfile
+
+    def line(self, children):
+        label = children[0]
+        statement = children[1]
+        # TODO: something more sensible:
+        return statement.replace('\t\t', '\t' + label + '\t')
+
+    def label(self, children):
+        return f'{children[0]}:'
+
+    def statement(self, children):
+        word = 0
+        text = []
+
+        for token in children:
+            if token in LOOKUP:
+                word |= LOOKUP[token]
+            elif isinstance(token, Token) and token.type in ('INT', 'SIGNED_INT'):
+                v = int(token)
+                v -= v < 0
+                word |= (v << 4) & MASK
+            text.append(str(token))
+
+            if isinstance(token, Token):
+                print(f'TOKEN: {token} ({token.type})')
+        statement = ', '.join(text)
+        label = comment = ''
+        return '\t'.join([f'{word:04X}', f'{word:06o}', label, statement, comment])
+
+
 def main():
     parser = argparse.ArgumentParser(description=DESC)
     parser.add_argument('source', help='Vector General instrutions to compile')
     parser.add_argument('-l', help='listing file name (.LST)')
     parser.add_argument('-o', help='output file name (.BIN)')
     args = parser.parse_args()
+    outfile = args.o
+    listfile = args.l
 
     sourcefile = args.source
     with open(sourcefile, 'r') as f:
         source = f.read()
     parser = Lark(vg_grammar)
     parse_tree = parser.parse(source)
+    transformer = VGTransformer(outfile, listfile)
+
+    print('Parse Tree:')
     print(parse_tree.pretty())
+    print('=' * 72)
+    print(transformer.transform(parse_tree).pretty())
 
 
 if __name__ == '__main__':
